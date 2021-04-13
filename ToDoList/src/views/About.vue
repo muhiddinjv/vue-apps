23f519dc29f10fb50529199af9678caedceabf49
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-04-13 15:31:24 +0500

    fix: replace new files with old ones after messing up

---
+++ ToDoList/src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the TodoList app v1.0.0. and this is just the beggining b/c I have
+      more app ideas in mind that I'm planning to implement soon.
+    </p>
+  </div>
+</template>
---
+++ ToDoList/src/views/Home.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "../components/Todos";
+import AddTodo from "../components/AddTodo";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    Todos,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then
+        // (res) => (this.todos = this.todos.filter((todo) => todo.id !== id)) //59:43
+        ()
+        .catch((err) => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
+    },
+  },
+  // 57:41
+  // https://jsonplaceholder.typicode.com/todos
+  created() {
+    //api rest request
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  border: none;
+  color: #fff;
+  display: inline-block;
+  background: #555;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
