51a5ef9847cd84f51488c5c628bbfde326a1f693
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-05-12 11:32:23 +0500

    init: start nuxtjokes project

---
+++ nuxtjokes/pages/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <p>Here you can find all the info you need to contact us.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contact',
+}
+</script>
+
+<style>
+</style>
